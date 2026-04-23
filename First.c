5c65ca656f62caf676a4ab8a5b9e1adfa3ae6d33
AnujGawade01 <121755207+AnujGawade01@users.noreply.github.com>
2026-04-23 13:57:39 +0530

    Update First.c

--- First.c
+++ First.c
@@ -1,6 +1,7 @@
+//Here return 0 is not written but the compiler will still add return 0 during compilation so the code will work
 #include<stdio.h>
 
 int main()
 {
-    printf("Marvellous Infosystems\n");
+    printf("Dennis Ritchie\n");
 }
