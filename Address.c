a54f2cd60cf368e7cbf58ef2df059c1a5ca3a7eb
AnujGawade01 <121755207+AnujGawade01@users.noreply.github.com>
2026-04-23 13:51:54 +0530

    Add files via upload

---
+++ Address.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{   
+    char ch = 'A';
+    int i = 11;
+    float f = 90.99f;
+    double d = 78.67890;
+
+    printf("L Values of all variables\n");
+    
+    printf("%lu\n",&ch);
+    printf("%lu\n",&i);
+    printf("%lu\n",&f);
+    printf("%lu\n",&d);
+
+    return 0;
+}
---
+++ Demo.c
@@ -0,0 +1,9 @@
+#include<stdio.h>/*# is preprocessor directive keyword, include gives command to include*/
+
+
+int main()
+{
+    printf("Jay Ganesh...\n");
+
+    return 0;  
+}
