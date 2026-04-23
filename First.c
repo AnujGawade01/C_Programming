bd496a8493d8512250ba00269357fef4cb87f0e4
AnujGawade01 <121755207+AnujGawade01@users.noreply.github.com>
2026-04-23 14:00:26 +0530

    Update First.c

--- First.c
+++ First.c
@@ -1,4 +1,4 @@
-//Here return 0 is not written but the compiler will still add return 0 during compilation so the code will work
+// Here return 0 is not written but the compiler will still add return 0 during compilation so the code will work
 #include<stdio.h>
 
 int main()
