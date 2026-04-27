4113c9259a1e9a64938bafe2c4fc83d05035fd3d
AnujGawade01 <121755207+AnujGawade01@users.noreply.github.com>
2026-04-27 17:07:57 +0530

    Update 1stSecond.c

--- 1stSecond.c
+++ 1stSecond.c
@@ -1,4 +1,4 @@
-//created for C Programming Assignment 8 Q.No 8 together with First2nd.c
+//created for C Programming Assignment 8 Q.No 8 together with First2nd.c to compile together
 int Add(int a, int b)
 {
     int Ret = 0;
