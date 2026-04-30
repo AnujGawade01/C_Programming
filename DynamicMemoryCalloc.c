0b129b84a1e82a35b72a79fe1ffd711a5ad2ea67
AnujGawade01 <121755207+AnujGawade01@users.noreply.github.com>
2026-04-30 14:44:35 +0530

    Add files via upload

---
+++ DynamicMemoryCalloc.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int Size = 0;
+    float *Marks = NULL;
+    int i = 0;           //Loop Counter
+
+    printf("Enter number of elements : \n");
+    scanf("%d",&Size);
+
+    //Dynamic Memory Allocation
+    Marks = (float *)calloc(Size , sizeof(float));
+
+    printf("Enter your Marks : \n");
+
+    //Iteration
+    //
+    for(i = 0; i < Size; i++)
+    {
+        scanf("%f",&Marks[i]);     //  4
+    }
+
+    printf("Entered Marks are : \n");
+
+    for(i = 0; i < Size; i++)
+    {
+        printf("%f\n",Marks[i]);     //  4
+    }
+
+    free(Marks);
+
+    return 0;
+    
+}
